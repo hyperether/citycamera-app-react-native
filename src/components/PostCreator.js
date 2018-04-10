d84b4be88e641e2ceccdaaf1e19c063905814822
Marina Radisavljevic <marinaradisavljevic@yahoo.com>
2018-04-10 17:47:02 +0200

    Implemented the purple square spinner in the PostCreator to give feedback while sending a post to the server.

--- src/components/PostCreator.js
+++ src/components/PostCreator.js
@@ -7,6 +7,7 @@ import ImageMenuItem from "./ImageMenuItem";
 import { Footer } from "./common";
 import TouchableMenuItem from "./TouchableMenuItem";
 import OverlayChooserItem from "./OverlayChooserItem";
+import OverlaySendingItem from "./OverlaySendingItem";
 import API from "../services/API";
 import Session from "../services/Session";
 import {loginUser, imageAdded, descriptionAdded, addLocation, postSent, logOut} from "../actions";
@@ -15,9 +16,11 @@ import {loginUser, imageAdded, descriptionAdded, addLocation, postSent, logOut} 
 class PostCreator extends Component {
   constructor(props) {
     super(props);
+    this.state = { sending: false, spinnerOn: false };
     console.log("props je", props)
     console.log('login user', this.props.loginUser)
   }
+
 
   logOutAlert(){
     Alert.alert(
@@ -41,25 +44,34 @@ class PostCreator extends Component {
   resetAllStates(){
     this.props.imageAdded({});
     this.props.descriptionAdded({});
-    this.props.addLocation({}); 
+    this.props.addLocation({});
   }
 
   onSendPress(){
     //Podaci prosledjeni iz redux-a
-    console.log("Image name:", this.props.imageName);    
+    console.log("Image name:", this.props.imageName);
     console.log("Image path:", this.props.imagePath);
     console.log("Image extension:", this.props.imageExtension);
     console.log("Description:", this.props.description);
     console.log("Longitude: ", this.props.longitude);
-    console.log("Latitude: ", this.props.latitude); 
+    console.log("Latitude: ", this.props.latitude);
     console.log("User je:", Session.getUser());
-  
+
+    this.setState( { sending: true });
+    this.disableAllMenutItems();
+    setInterval(() => {
+       this.setState(previousState => {
+         return {
+           spinnerOn: !previousState.spinnerOn,
+        };
+      });
+    }, 1000);
     var uploadURL, fileId;
-  
+
     API.getUploadURL(
-      this.props.imageName, 
+      this.props.imageName,
       this.props.imageExtension,
-      this.props.description, 
+      this.props.description,
       {long: this.props.longitude, lat: this.props.lat},
     ).then (response => {
       uploadURL = response.data.url;
@@ -71,29 +83,63 @@ class PostCreator extends Component {
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
             // console.log("Successfully uploaded the file.");
-            ToastAndroid.show("Successfully uploaded the file.", ToastAndroid.LONG);            
+            this.setState({ sending: false });
+            ToastAndroid.show("Successfully uploaded the file.", ToastAndroid.LONG);
             this.resetAllStates();
           } else {
             // console.log("The file could not be uploaded.");
-            ToastAndroid.show("The file could not be uploaded.", ToastAndroid.LONG);            
-            
+            this.setState({ sending: false });
+            ToastAndroid.show("The file could not be uploaded.", ToastAndroid.LONG);
+
           }
         }
       }
       xhr.open('PUT', uploadURL)
       // xhr.setRequestHeader('X-Amz-ACL', 'public-read')
       xhr.setRequestHeader('Content-Type', this.props.imageExtension)
-      xhr.send({ 
-        uri: 'file://'+this.props.imagePath, 
-        name: fileId 
+      xhr.send({
+        uri: 'file://'+this.props.imagePath,
+        name: fileId
       })
-    }) 
+    })
+  }
+
+  disableAllMenutItems() {
+    this.renderItem("photo", false);
+    this.renderItem("location", false);
+    this.renderItem("description", false);
+    this.renderItem("send", false);
+  }
+
+
+  renderSpinner() {
+    if (this.state.sending) {
+      if (this.state.spinnerOn) {
+        return (
+          <OverlaySendingItem
+            imagePath={require("../assets/images/square.png")}
+          />
+        );
+      } else {
+        return (
+          <OverlaySendingItem
+            imagePath={require("../assets/images/square_rotate.png")}
+          />
+        );
+      }
+    } else {
+      return (
+        <OverlayChooserItem
+          imagePath={require("../assets/images/square.png")}
+        />
+      );
+    }
   }
 
   renderItem(type, isTouchable) {
     switch (type) {
       case "photo":
-        if (isTouchable) {
+        if (!this.state.sending && isTouchable) {
           return (
             <TouchableMenuItem
               imagePath={require("../assets/images/photo.png")}
@@ -108,7 +154,7 @@ class PostCreator extends Component {
           );
         }
       case "location":
-        if (isTouchable) {
+        if (!this.state.sending && isTouchable) {
           return (
             <TouchableMenuItem
               imagePath={require("../assets/images/location.png")}
@@ -123,7 +169,7 @@ class PostCreator extends Component {
           );
         }
       case "description":
-        if (isTouchable) {
+        if (!this.state.sending && isTouchable) {
           return (
             <TouchableMenuItem
               imagePath={require("../assets/images/about.png")}
@@ -139,7 +185,7 @@ class PostCreator extends Component {
         }
 
       case "send":
-        if (isTouchable) {
+        if (!this.state.sending && isTouchable) {
           return (
             <TouchableMenuItem
               imagePath={require("../assets/images/send.png")}
@@ -167,9 +213,7 @@ class PostCreator extends Component {
     return (
       <View style={mainContainerStyle}>
         <View style={menuItemsContainerStyle}>
-          <OverlayChooserItem
-            imagePath={require("../assets/images/square.png")}
-          />
+          {this.renderSpinner()}
           <View style={menuRowStyle}>
             {this.renderItem("photo", true)}
             {this.renderItem("location", this.props.imageName)}
@@ -221,8 +265,8 @@ class PostCreator extends Component {
   }
 
   //za primanje stateova u props-e
-  //<-- da bi prisli nekom od ovih propseva kucamo this.props.imagePath, npr  
-  const mapStateToProps = state => { 
+  //<-- da bi prisli nekom od ovih propseva kucamo this.props.imagePath, npr
+  const mapStateToProps = state => {
     return {
       imagePath: state.post.imagePath,
       imageName: state.post.imageName,
