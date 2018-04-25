55e80d383c3175f7bf61df26860a1ebeca7e7c5a
hyperdev <hyperdev@Slobodans-MacBook-Pro.local>
2018-04-25 13:32:09 +0200

    Added Google Maps for iOS.

--- ios/citycamera/AppDelegate.m
+++ ios/citycamera/AppDelegate.m
@@ -10,12 +10,16 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+@import GoogleMaps;
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
 
+  [GMSServices provideAPIKey:@"IzaSyDnq2qV7IjyB2N2lCHoCS7V41e27JcAoJU"];
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
